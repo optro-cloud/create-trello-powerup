9a5472f2427ad8bc2716b3ae98cf97f56e020459
jeffortlessly <jeff.tomband@automation-consultants.com>
2021-07-21 16:20:04 +0200

    :art: Updated templates and added to package.json

--- templates/router.tsx
+++ templates/router.tsx
@@ -1,15 +1,20 @@
 import React, { Suspense } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {TrelloProvider} from '@optro/ui-react';
 
 // Lazy Loaders
+
+const t = window.TrelloPowerUp.iframe();
 
 function PowerupRouter() {
   return (
     <div>
+      <TrelloProvider t={t}>
       <Suspense fallback={<div style={{ margin: '6px' }}>Loading...</div>}>
         <Router basename={process.env.CONTEXT_PATH || undefined}>
         </Router>
       </Suspense>
+      </TrelloProvider>
     </div>
   );
 }
